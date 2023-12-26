4a4d4cbab9a3b3f543b2fc0d949c2f369b46aa20
ICHWAL <145094989+ichwalmr@users.noreply.github.com>
2023-12-26 23:48:52 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,52 @@ const mengetik = new Typed(".mengetik", {
   backSpeed: 20,
   loop: true,
 });
+    document.addEventListener("DOMContentLoaded", function () {
+      const colectionContainers = document.querySelectorAll(
+        ".colection-container"
+      );
+      const modal = document.getElementById("myModal");
+      const modalImg = document.getElementById("modalImg");
+
+      colectionContainers.forEach(function (container) {
+        container.addEventListener("click", function (event) {
+          event.stopPropagation(); // Stop the click event from propagating to the container
+          const imgSrc = container.querySelector("img").src;
+          modal.style.display = "block";
+          modalImg.innerHTML = `<img src="${imgSrc}" alt="enlarged image">`;
+        });
+      });
+
+      modal.addEventListener("click", function () {
+        modal.style.display = "none";
+      });
+    });
+
+    const sendgrid = require("@sendgrid/mail");
+
+    exports.handler = async function (event, context) {
+      try {
+        const { name, email, message } = JSON.parse(event.body);
+
+        sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+        const msg = {
+          to: "pekkamax697@gmail.com",
+          from: email,
+          subject: "New Contact Form Submission",
+          text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
+        };
+
+        await sendgrid.send(msg);
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: "Message sent successfully!" }),
+        };
+      } catch (error) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ message: "Error sending message." }),
+        };
+      }
+    };
